0e9c13c116a42c01328f6e018a522c9eac205cff
The-MG <63264652+The-MG@users.noreply.github.com>
2021-08-19 21:02:12 -0700

    Update API.php

--- src/API.php
+++ src/API.php
@@ -4,22 +4,31 @@ namespace Force\API;
 use GuzzleHttp\Client;
 
 class API {
-	$client = new GuzzleHttp\Client(['base_uri' => 'https://api.fallback.forcehost.net/']);
+
+	    public function __construct(Client $guzzle = null)
+    {
+        $this->guzzle = $guzzle ?: new Client([
+            'base_uri'    => 'https://api.fallback.forcehost.net/',
+            'http_errors' => false,
+        ]);
+
+        return $this;
+    }
 
 	public function GetUserRam ($userid) {
-		$response = $client->request('GET', "userram.php?id=$userid");
+		$response = $this->request('GET', "userram.php?id=$userid");
 		return $response->getBody();
 	}
 	public function GetUserCpu ($userid) {
-		$response = $client->request('GET', "usercpu.php?id=$userid");
+		$response = $this->request('GET', "usercpu.php?id=$userid");
 		return $response->getBody();
 	}
 	public function GetUserDisk ($userid) {
-		$response = $client->request('GET', "userdisk.php?id=$userid");
+		$response = $this->request('GET', "userdisk.php?id=$userid");
 		return $response->getBody();
 	}
 	public function GetUserSlots ($userid) {
-		$response = $client->request('GET', "userslots.php?id=$userid");
+		$response = $this->request('GET', "userslots.php?id=$userid");
 		return $response->getBody();
 	}
 }
