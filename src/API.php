ec2fb9b1171b21b08c66c04f3f625d0c17dc65b7
The-MG <63264652+The-MG@users.noreply.github.com>
2021-08-19 21:14:53 -0700

    update

--- src/API.php
+++ src/API.php
@@ -1,34 +1,24 @@
 <?php
 namespace Force\API;
 
-use GuzzleHttp\Client;
+use Force\API\Http;
 
 class API {
 
-	    public function __construct(Client $guzzle = null)
-    {
-        $this->guzzle = $guzzle ?: new Client([
-            'base_uri'    => 'https://api.fallback.forcehost.net/',
-            'http_errors' => false,
-        ]);
-
-        return $this;
-    }
-
 	public function GetUserRam ($userid) {
-		$response = $this->request('GET', "userram.php?id=$userid");
+		$response = $this->http->get("userram.php?id=$userid");
 		return $response->getBody();
 	}
 	public function GetUserCpu ($userid) {
-		$response = $this->request('GET', "usercpu.php?id=$userid");
+		$response = $this->http->get("usercpu.php?id=$userid");
 		return $response->getBody();
 	}
 	public function GetUserDisk ($userid) {
-		$response = $this->request('GET', "userdisk.php?id=$userid");
+		$response = $this->http->get("userdisk.php?id=$userid");
 		return $response->getBody();
 	}
 	public function GetUserSlots ($userid) {
-		$response = $this->request('GET', "userslots.php?id=$userid");
+		$response = $this->http->get('GET', "userslots.php?id=$userid");
 		return $response->getBody();
 	}
 }
