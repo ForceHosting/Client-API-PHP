2b911c6e9cb7c684b876544eba3f8a377873fc76
The-MG <63264652+The-MG@users.noreply.github.com>
2021-08-19 22:09:21 -0700

    Official Release

--- src/API.php
+++ src/API.php
@@ -1,24 +1,59 @@
 <?php
 namespace Force\API;
 
-use Force\API\Http;
+use GuzzleHttp\Client;
+//use Force\API\Http;
 
 class API {
 
-	public function GetUserRam ($userid) {
-		$response = $this->http->get("userram.php?id=$userid");
-		return $response->getBody();
-	}
-	public function GetUserCpu ($userid) {
-		$response = $this->http->get("usercpu.php?id=$userid");
-		return $response->getBody();
-	}
-	public function GetUserDisk ($userid) {
-		$response = $this->http->get("userdisk.php?id=$userid");
-		return $response->getBody();
-	}
-	public function GetUserSlots ($userid) {
-		$response = $this->http->get('GET', "userslots.php?id=$userid");
-		return $response->getBody();
-	}
+   /**
+     * The GuzzleHttp client.
+     *
+     * @var Client
+     */
+    protected $guzzle;
+
+    /**
+     * Number of seconds a request is retried.
+     *
+     * @var int
+     */
+    public $timeout = 30;
+
+    public function __construct(Client $guzzle = null)
+    {
+        $this->guzzle = $guzzle ?: new Client([
+            'base_uri'    => 'https://api.fallback.forcehost.net/',
+            'http_errors' => false,
+        ]);
+        return $this;
+    }
+    /**
+     * Make request and return the response.
+     *
+     * @param string $method
+     * @param string $uri
+     * @param array  $query
+     * @param array  $payload
+     *
+     * @return mixed
+     */
+    public function request($uri)
+    {
+        $response = $this->guzzle->request('GET', "https://api.fallback.forcehost.net/".$uri);
+        $responseBody = (string) $response->getBody();
+        return $responseBody;
+    }
+    public function GetUserRam ($userid) {
+        return $this->request("userram.php?id=$userid");
+    }
+    public function GetUserCpu ($userid) {
+        return $this->request("usercpu.php?id=$userid");
+    }
+    public function GetUserDisk ($userid) {
+        return $this->request("userdisk.php?id=$userid");
+    }
+    public function GetUserSlots ($userid) {
+        return $this->request("userslots.php?id=$userid");
+    }
 }
